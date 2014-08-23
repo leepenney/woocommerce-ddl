82bdb9e8c2afcb24ebebb61eb818222c08e458f8
leepenney <leepenney@users.noreply.github.com>
2014-08-23 17:19:49 +0100

    Changed locations on a few items and added additional ones

--- woocommerce-ddl.php
+++ woocommerce-ddl.php
@@ -16,15 +16,30 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	global $ddl;
 	$ddl = array();
 	
+	function woo_ddl_page_details() {
+		global $post, $ddl;
+		
+		$ddl['page']['pageInfo']['pageName'] = esc_js($post->post_title);
+		$ddl['page']['pageInfo']['pageID'] = esc_js($post->post_title);
+		$ddl['page']['pageInfo']['destinationURL'] = esc_js(get_permalink());
+
+	}
+	add_action('wp_footer', 'woo_ddl_page_details');
+	
 	function woo_ddl_single_product_details() {
 		global $post, $ddl;
 
 		$product = get_product( $post->ID );
 		
-		$ddl['page']['pageInfo']['pageName'] = esc_js($post->post_title);
-		$ddl['page']['category']['pageType'] = 'product';
+		$product_categories = array_values(wp_get_post_terms($post->ID, 'product_cat', array('orderby' => 'parent')));
 		
-		$ddl['product'][0]['productInfo']['productID'] = $post->ID;
+		$ddl['page']['category']['pageType'] = 'product';
+		$ddl['page']['category']['primaryCategory'] = esc_js($product_categories[0]->name);
+		if ($product_categories[1]) {
+			$ddl['page']['category']['subCategory1'] = esc_js($product_categories[1]->name);
+		}
+		
+		$ddl['product'][0]['productInfo']['productID'] = (string)$post->ID;
 		$ddl['product'][0]['productInfo']['productName'] = esc_js($post->post_title);
 		$ddl['product'][0]['productInfo']['description'] = esc_js($post->post_content);
 		$ddl['product'][0]['productInfo']['productURL'] = esc_js(get_permalink());
@@ -37,62 +52,66 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		
 		$sku = $product->get_sku();
 		if (strlen($sku) > 0) {
-			$ddl['product'][0]['productInfo']['sku'] = $sku;
+			$ddl['product'][0]['productInfo']['sku'] = (string)$sku;
 		}
 		
-		$product_categories = get_the_terms($post->ID, 'product_cat');
-		$first_cat = array_shift($product_categories);
+		$ddl['product'][0]['category']['primaryCategory'] = esc_js($product_categories[0]->name);
+		if ($product_categories[1]) {
+			$ddl['product'][0]['category']['subCategory1'] = esc_js($product_categories[1]->name);
+		}
 		
-		$ddl['product'][0]['category']['primaryCategory'] = esc_js($first_cat->name);
-		$ddl['product'][0]['price']['price'] = $product->get_price();
+		$ddl['product'][0]['price']['currency'] = get_woocommerce_currency();
+		$ddl['product'][0]['price']['price'] = (double)$product->get_price();
+		$ddl['product'][0]['price']['priceWithTax'] = $product->get_price_including_tax();
+		$ddl['product'][0]['price']['basePrice'] = $product->get_price_excluding_tax();
 		
 		if ($product->is_on_sale()) {
-			$ddl['product'][0]['price']['regular_unit_price'] = $product->get_regular_price();
+			$ddl['product'][0]['price']['regular_unit_price'] = (double)$product->get_regular_price();
 		}
 		
 		if ($product->get_stock_quantity()) {
 			$ddl['product'][0]['attributes']['stock'] = $product->get_stock_quantity();
+		} else {
+			$ddl['product'][0]['attributes']['stock'] = $product->is_in_stock() ? 1 : 0;
 		}
 	}
 	add_action('woocommerce_after_single_product', 'woo_ddl_single_product_details');
 	
 	function woo_ddl_cart_details() {
-		global $woocommerce, $ddl;
+		global $woocommerce, $wp, $ddl;
 		
 		$ddl['page']['category']['pageType'] = 'basket';
 		
-		$ddl['cart']['price']['basePrice'] = preg_replace("/&#?[a-z0-9]+;/i","", strip_tags($woocommerce->cart->get_cart_subtotal()));
-		$ddl['cart']['price']['cartTotal'] = preg_replace("/&#?[a-z0-9]+;/i","",strip_tags($woocommerce->cart->get_cart_total()));
-		
-		$shipping = $woocommerce->cart->get_cart_shipping_total();
-		if (strpos($shipping,'Free') !== FALSE) {
-			$shipping = '0.00';
-		}
-		$ddl['cart']['price']['shipping'] = $shipping;
+		$ddl['cart']['price']['currency'] = get_woocommerce_currency();
+		$ddl['cart']['price']['basePrice'] = $woocommerce->cart->subtotal_ex_tax;
+		$ddl['cart']['price']['cartTotal'] = $woocommerce->cart->total;
+		$ddl['cart']['price']['priceWithTax'] = $woocommerce->cart->subtotal;
+		$ddl['cart']['price']['shipping'] = $woocommerce->cart->shipping_total;
 		
 		$ddl['cart']['item'] = array();
 		
-		$cart_contents = $woocommerce->cart->get_cart();
-		foreach ($cart_contents as $item) {
-			$product = get_product($item['product_id']);
+		foreach ($woocommerce->cart->get_cart() as $item_key => $item) {
 			$item_details = array();
 
-			$item_details['productInfo']['productID'] = $item['product_id'];
+			$item_details['productInfo']['productID'] = (string)$item['product_id'];
 			$item_details['productInfo']['productName'] = esc_js($item['data']->post->post_title);
 			$item_details['productInfo']['description'] = esc_js($item['data']->post->post_content);
+			$item_details['productInfo']['productURL'] = esc_js($item['data']->get_permalink());
+			
 			$item_details['quantity'] = $item['quantity'];
 			$item_details['price']['basePrice'] = $item['line_subtotal']/$item['quantity'];
 			$item_details['price']['priceWithTax'] = $item['line_total']/$item['quantity'];
+			$item_details['price']['currency'] = get_woocommerce_currency();
 			
-			$product_image_id = $product->get_image_id();
+			$product_image_id = $item['data']->get_image_id();
 			if ($product_image_id) {
 				$item_details['productInfo']['productThumbnail'] = esc_js(woo_ddl_get_image_src($product_image_id, 'shop_thumbnail'));
 				$item_details['productInfo']['productImage'] = esc_js(woo_ddl_get_image_src($product_image_id, 'full'));
 			}
-			
-			$sku = $product->get_sku();
+
+			$sku = $item['data']->get_sku();
 			if (strlen($sku) > 0) {
-				$item_details['productInfo']['sku'] = $sku;
+				$item_details['productInfo']['sku'] = (string)$sku;
 			}
 			
 			array_push($ddl['cart']['item'], $item_details);
@@ -100,25 +119,37 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	}
 	add_action('woocommerce_after_cart', 'woo_ddl_cart_details');
 	
-	function woo_ddl_purchase_confirmation() {
+	function woo_ddl_purchase_confirmation($order_id) {
 		global $ddl;
-		$ddl['page']['category']['pageType'] = 'confirmation';
+		$order = new WC_Order( $order_id );
+		
+		if ($order->status != 'failed') {
+			// only display if the order succeeded
+			$ddl['page']['category']['pageType'] = 'confirmation';
+			$ddl['transaction']['transactionID'] = $order_id;
+		}
 	}
 	add_action('woocommerce_thankyou', 'woo_ddl_purchase_confirmation');
 	
 	function woo_ddl_checkout() {
-		global $ddl, $order;
+		global $ddl;
 		$ddl['page']['category']['pageType'] = 'checkout';
-		$ddl['transaction']['transactionID'] = $order->get_order_number();
 	}
 	add_action('woocommerce_after_checkout_form', 'woo_ddl_checkout');
 	
 	function woo_ddl_user_info() {
 		if (is_user_logged_in()) {
 			//if the user is logged in
-			global $current_user, $post, $ddl;
+			global $current_user, $ddl;
 			
 			$ddl['user'][0]['profile'][0]['profileInfo']['email'] = $current_user->user_email;
+			if (strlen($current_user->user_firstname) > 0) {
+				$name = $current_user->user_firstname;
+				
+				if (strlen($current_user->user_lastname) > 0) $name .= ' '.$current_user->user_lastname;
+				
+				$ddl['user'][0]['profile'][0]['profileInfo']['userName'] = $name;
+			}
 		}
 		woo_ddl_print_ddl_variable();
 	}
@@ -137,4 +168,5 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		echo 'window.digitalData = ',json_encode($ddl),"\n";
 		echo '</script>',"\n";
 	}
+
 }
